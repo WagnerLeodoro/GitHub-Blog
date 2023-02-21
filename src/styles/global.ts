59ea004087263ea7f51550029ef7a96144b00322
WagnerLeodoro <wagner.augustoleodoro@gmail.com>
2023-02-20 23:47:22 -0300

    Estilos globais e tema da aplicação

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors["blue"]}
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.colors["base-background"]};
+    color: ${({ theme }) => theme.colors["base-text"]};
+    -webkit-font-smoothing: antialiased;
+    line-height: 160%;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem Nunito, sans-serif;
+    line-height: 160%;
+  }
+`;
